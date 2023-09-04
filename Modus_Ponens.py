be3cd455aed721c7a73687b38a95881f9bc5a561
MitziEHZ0325 <esme1313dora@gmail.com>
2023-09-03 19:18:43 -0600

    Codigo Modus Ponens

---
+++ Modus_Ponens.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 18:59:32 2023
+
+@author: Mitzi
+"""
+
+metro_funciona_correctamente = True
+
+llegar_a_tiempo = "Llegarás a tiempo a tu compromiso importante"
+
+if metro_funciona_correctamente:
+    print(llegar_a_tiempo)
+else:
+    print("No se puede inferir si llegarás a tiempo o no")
