98f643ced70f1894adf1844f5faf784a50cd4c3f
MitziEHZ0325 <esme1313dora@gmail.com>
2023-09-03 19:19:14 -0600

    Codigo Modus Tollens

---
+++ Modus_Tollens.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 19:01:53 2023
+
+@author: Mitzi
+"""
+
+llegar_tarde = "Llegaste tarde a tu compromiso importante"
+
+metro_no_funciona_correctamente = True
+
+if llegar_tarde:
+    print("El metro no funciona correctamente")
+else:
+    print("No se puede inferir si el metro funciona correctamente o no")
